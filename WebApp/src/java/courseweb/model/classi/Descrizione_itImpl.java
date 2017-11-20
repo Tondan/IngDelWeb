fe4a1dfc425dd89a8769c1a75c5c3692642c531a
Unknown <tunyx7@gmail.com>
2017-11-21 00:45:00 +0100

    up robetta

---
+++ WebApp/src/java/courseweb/model/classi/Descrizione_itImpl.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package courseweb.model.classi;
+import courseweb.model.interfacce.*;
+
+/**
+ *
+ * @author Toni & Tony
+ */
+public class Descrizione_itImpl implements Descrizione_it{
+    
+    private int corso;
+    
+    private String prerequisiti;
+    
+    private String obiettivi;
+    
+    private String mod_esame;
+    
+    private String mod_insegnamento;
+    
+    private String sillabo;
+    
+    private String note;
+    
+    private String homepage;
+    
+    private String forum;
+    
+    private String risorse_ext;
+    
+    
+    
+    public Descrizione_itImpl(){
+    this.corso=0;
+    this.prerequisiti=null;
+    this.obiettivi=null;
+    this.mod_esame=null;
+    this.mod_insegnamento=null;
+    this.sillabo=null;
+    this.note=null;
+    this.homepage=null;
+    this.forum=null;
+    this.risorse_ext=null;
+}
+
+    @Override
+    public int getCorso(int id_corso) {
+        return this.corso;
+    }
+
+    @Override
+    public void setCorso(int id_corso) {
+        this.corso=id_corso;
+    }
+
+    @Override
+    public String getPrerequisiti() {
+        return this.prerequisiti;
+    }
+
+    @Override
+    public void setPrerequisiti(String prerequisiti) {
+        this.prerequisiti=prerequisiti;
+    }
+
+    @Override
+    public String getObiettivi() {
+        return this.obiettivi;
+    }
+
+    @Override
+    public void setObiettivi(String obiettivi) {
+        this.obiettivi=obiettivi;
+    }
+
+    @Override
+    public String getMod_Esame() {
+        return this.mod_esame;
+    }
+
+    @Override
+    public void setMod_Esame(String mod_esame) {
+        this.mod_esame=mod_esame;
+    }
+
+    @Override
+    public String getMod_Insegnamento() {
+        return this.mod_insegnamento;
+    }
+
+    @Override
+    public void setMod_Insegnamento(String mod_insegnamento) {
+        this.mod_insegnamento=mod_insegnamento;
+    }
+
+    @Override
+    public String getSillabo() {
+        return this.sillabo;
+    }
+
+    @Override
+    public void setSillabo(String sillabo) {
+        this.sillabo=sillabo;
+    }
+
+    @Override
+    public String getNote() {
+        return this.note;
+    }
+
+    @Override
+    public void setNote(String note) {
+        this.note=note;
+    }
+
+    @Override
+    public String getHomepage() {
+        return this.homepage;
+    }
+
+    @Override
+    public void setHomepage(String Homepage) {
+        this.homepage=Homepage;
+    }
+
+    @Override
+    public String getForum() {
+        return this.forum;
+    }
+
+    @Override
+    public void setForum(String forum) {
+        this.forum=forum;
+        
+    }
+
+    @Override
+    public String getRisorse_Ext() {
+        return this.risorse_ext;
+    }
+
+    @Override
+    public void setRisorse_Ext(String risorse) {
+        this.risorse_ext=risorse;
+    }
+}
---
+++ WebApp/src/java/courseweb/model/classi/DocenteImpl.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package courseweb.model.classi;
+import courseweb.model.interfacce.*;
+import courseweb.controller.data.DataLayerException;
+import java.util.List;
+/**
+ *
+ * @author Ton & Toni
+ */
+public class DocenteImpl implements Docente {
+    
+    private int id_docente;
+    
+    private String immagine;
+    
+    private String nome;
+    
+    private String cognome;
+    
+    private String email;
+    
+    private String ufficio;
+    
+    private String telefono;
+    
+    private String specializzazione;
+    
+    private String ricerche;
+    
+    private String pubblicazione;
+    
+    private String curriculum;
+    
+    private String ricevimento;
+    
+    private List<Corso> corsi;
+    
+    
+    public DocenteImpl(){
+        this.id_docente=0;
+        this.nome=null;
+        this.cognome=null;
+        this.email=null;
+        this.ufficio=null;
+        this.telefono=null;
+        this.specializzazione=null;
+        this.ricerche=null;
+        this.pubblicazione=null;
+        this.curriculum=null;
+        this.ricevimento=null;
+    }
+    
+    
+    @Override
+    public int getIDDocente(){
+        return this.id_docente;
+    }
+
+    @Override
+    public String getImmagine() {
+        return this.immagine;
+    }
+
+    @Override
+    public String getNome() {
+        return this.nome;
+    }
+
+    @Override
+    public String getCognome() {
+        return this.cognome;
+    }
+
+    @Override
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public String getUfficio() {
+        return this.ufficio;
+    }
+
+    @Override
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    @Override
+    public String getSpecializzazione() {
+        return this.specializzazione;
+    }
+
+    @Override
+    public String getRicerche() {
+        return this.ricerche;
+    }
+
+    @Override
+    public String getPubblicazione() {
+       return this.pubblicazione;
+    }
+
+    @Override
+    public String getCurriculum() {
+        return this.curriculum;
+    }
+
+    @Override
+    public String getRicevimento() {
+        return this.ricevimento;
+    }
+
+    @Override
+    public void setImmagine(String Immagine) {
+        this.immagine=Immagine;
+    }
+    
+
+    @Override
+    public void setNome(String nome) {
+        this.nome=nome;
+    }
+
+    @Override
+    public void setCognome(String cognome) {
+        this.cognome=cognome;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email=email;
+    }
+
+    @Override
+    public void setUfficio(String ufficio) {
+       this.ufficio=ufficio;
+    }
+
+    @Override
+    public void setTelefono(String telefono) {
+        this.telefono=telefono;
+    }
+
+    @Override
+    public void setSpecializzazione(String specializzazione) {
+        this.specializzazione=specializzazione;    
+    }
+
+    @Override
+    public void setRicerche(String ricerche) {
+        this.ricerche=ricerche;
+    }
+
+    @Override
+    public void setPubblicazioni(String pubblicazioni) {
+        this.pubblicazione=pubblicazioni;
+    
+    }
+
+    @Override
+    public void setCurriculum(String curriculum) {
+        this.curriculum=curriculum;
+    }
+
+    @Override
+    public void setRicevimento(String ricevimento) {
+        this.ricevimento=ricevimento;
+    }
+
+    @Override
+    public List<Corso> getCorsi() throws DataLayerException {
+        return this.corsi;
+    }
+
+        @Override
+    public void setCorsi(List<Corso> corsi){
+        this.corsi=corsi;
+    }
+    
+    @Override
+    public void addCorso(Corso corso){
+        this.corsi.add(corso);
+    }
+    
+}
