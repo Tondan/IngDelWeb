2925490790986384f871f1cfe87a07c861606eb3
ChrisMatto <christian-toni@hotmail.it>
2017-11-22 15:09:53 +0100

    igw

--- WebApp/src/java/courseweb/model/classi/IgwDataLayerMysqlImpl.java
+++ WebApp/src/java/courseweb/model/classi/IgwDataLayerMysqlImpl.java
@@ -5,10 +5,164 @@
  */
 package courseweb.model.classi;
 
+import courseweb.controller.data.DataLayerException;
+import courseweb.controller.data.DataLayerMysqlImpl;
+import courseweb.model.interfacce.CDL;
+import courseweb.model.interfacce.Corso;
+import courseweb.model.interfacce.Descrizione_en;
+import courseweb.model.interfacce.Descrizione_it;
+import courseweb.model.interfacce.Docente;
+import courseweb.model.interfacce.Dublino_en;
+import courseweb.model.interfacce.Dublino_it;
+import courseweb.model.interfacce.Gruppo;
+import courseweb.model.interfacce.IgwDataLayer;
+import courseweb.model.interfacce.Libro;
+import courseweb.model.interfacce.Log;
+import courseweb.model.interfacce.Materiale;
+import courseweb.model.interfacce.Servizio;
+import courseweb.model.interfacce.Utente;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 /**
  *
  * @author Toni & Tony
  */
-public class IgwDataLayerMysqlImpl {
+public class IgwDataLayerMysqlImpl extends DataLayerMysqlImpl implements IgwDataLayer {
+
+    public IgwDataLayerMysqlImpl(DataSource datasource) throws SQLException, NamingException {
+        super(datasource);
+    }
+
+    @Override
+    public CDL createCDL() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Corso createCorso() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Docente createDocente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Descrizione_it createDescrizione_it() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Descrizione_en createDescrizione_en() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dublino_it createDublino_it() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dublino_en createDublino_en() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Materiale createMateriale() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Libro createLibro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Gruppo createGruppo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Utente createUtente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Servizio createServizio() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Log CreateLog() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CDL getCDL(int IDCdl) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Corso getCorso(int IDCorso) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Docente getDocente(int IDDocente) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Descrizione_it getDescrizione_it(int Corso) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Descrizione_en getDescrizione_en(int Corso) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dublino_it getDublino_it(int Corso) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dublino_en getDublino_en(int Corso) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Materiale getMateriale(int IDLibro) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Libro getLibro(int IDLibro) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Gruppo getGruppo(int IDGruppo) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Utente getUtente(int IDUtente) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Servizio getServizio(int IDServizio) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Log getLog(int IDLog) throws DataLayerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
