83357da432f80877d1fe413f66c651355c907690
Unknown <tunyx7@gmail.com>
2017-12-20 00:34:07 +0100

    Nice

--- WebApp/src/java/courseweb/controller/DetailsDocente.java
+++ WebApp/src/java/courseweb/controller/DetailsDocente.java
@@ -8,6 +8,10 @@ import courseweb.model.interfacce.IgwDataLayer;
 import courseweb.view.FailureResult;
 import courseweb.view.TemplateManagerException;
 import courseweb.view.TemplateResult;
+import courseweb.view.StreamResult;
+import java.io.File;
+import java.net.URL;
+import java.net.URLConnection;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -39,6 +43,38 @@ public class DetailsDocente extends BaseController {
             action_error(request, response);
         }
     }
+    
+    private void action_download(HttpServletRequest request, HttpServletResponse response, int res) throws IOException, ServletException, TemplateManagerException {
+        StreamResult result = new StreamResult(getServletContext());
+        //in base a res, dovremmo determinare la risorsa da scaricare, quindi aprire uno stream di input
+        //per leggerla in binario
+        //...
+        //con la classe StreamResult possiamo usare un file o direttamente uno stream, anche aperto da una base di dati
+        //qui, per esempio, prendiamo sempre un file di test all'interno della nostra applicazione
+        //we should determine the file to download using the res parameter, but in this toy example we always download the same file
+        //the StreamResult utility class provides methods to start a binary download from a file or any data stream
+        //
+        //usate questa versione per leggere una risorsa incorporata nel WAR e se siete certi che sia stato espanso sul disco, o per una risorsa prelevata da una cartella esterna al contesto
+        //use this version only if you want to read a resource embedded in the WAR file AND you know that it has been expanded to disk, or to read a resource from a folder outside the context
+        File in = new File(getServletContext().getRealPath("") + File.separatorChar + "curriculum.txt");
+        result.activate(in, request, response);
+        //
+        //usate questa versione per leggere una risorsa incorporata nel WAR
+        //use this version to read a resource is inside the WAR
+//        URL resource = getServletContext().getResource("/" + "file_di_esempio.txt");
+//        if (resource != null) {
+//            URLConnection connection = resource.openConnection();
+//            String url = resource.toString();
+//            //settiamo il tipo del file da trasmettere
+//            //set the media type of the file to send
+//            request.setAttribute("contentType", connection.getContentType());
+//            result.activate(connection.getInputStream(), connection.getContentLength(), url.substring(url.lastIndexOf('/') + 1), request, response);
+//        } else {
+//            request.setAttribute("exception", new FileNotFoundException("Resource not found: "+res));
+//            action_error(request, response);
+//        }
+    }
+    
 
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -48,17 +84,19 @@ public class DetailsDocente extends BaseController {
         try {
             k = SecurityLayer.checkNumeric(request.getParameter("k"));
             action_default(request, response, k);
+            int res = SecurityLayer.checkNumeric(request.getParameter("res"));
+            action_download(request, response, res);
+    
         } catch (NumberFormatException ex) {
             request.setAttribute("message", "Teacher key not specified");
             action_error(request, response);
         } catch (IOException ex) {
             request.setAttribute("exception", ex);
             action_error(request, response);
-
+               
         } catch (TemplateManagerException ex) {
             request.setAttribute("exception", ex);
             action_error(request, response);
-
         }
     }
 
