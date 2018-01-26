c960217dfc9332829c4b29b4a966a66081e09625
ChrisMatto <christian-toni@hotmail.it>
2018-01-26 11:21:06 +0100

    ok

--- WebApp/src/java/courseweb/controller/RegisterDocente.java
+++ WebApp/src/java/courseweb/controller/RegisterDocente.java
@@ -1,23 +1,68 @@
-
 package courseweb.controller;
 
+import courseweb.controller.data.DataLayerException;
+import courseweb.controller.security.SecurityLayer;
+import courseweb.model.interfacce.Corso;
+import courseweb.model.interfacce.Descrizione_it;
+import courseweb.model.interfacce.Docente;
+import courseweb.model.interfacce.Dublino_it;
+import courseweb.model.interfacce.IgwDataLayer;
+import courseweb.model.interfacce.Libro;
+import courseweb.view.FailureResult;
+import courseweb.view.TemplateManagerException;
+import courseweb.view.TemplateResult;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Tony
+ * @author Toni & Tony
  */
 public class RegisterDocente extends BaseController {
 
+    private void action_error(HttpServletRequest request, HttpServletResponse response) {
+        if (request.getAttribute("exception") != null) {
+            (new FailureResult(getServletContext())).activate((Exception) request.getAttribute("exception"), request, response);
+        } else {
+            (new FailureResult(getServletContext())).activate((String) request.getAttribute("message"), request, response);
+        }
+    }
+
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+        HttpSession session= request.getSession(false);
+        String a = (String) session.getAttribute("username");
+        request.setAttribute("nome",a);
+        TemplateResult res = new TemplateResult(getServletContext());
+        request.setAttribute("page_title", "Backoffice");
+        res.activate("register_docente.ftl.html", request, response); 
+    }
+    
+    
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-        
+
+       
+        try {
+                action_default(request, response);  
+        } catch (IOException | TemplateManagerException ex) {
+            request.setAttribute("exception", ex);
+            action_error(request, response);
+
+        }
     }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Main Login servlet";
+    }// </editor-fold>
+
 }
-    
-    
