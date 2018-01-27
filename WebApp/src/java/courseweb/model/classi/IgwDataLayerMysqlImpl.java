91d5689b84e3392390a9158ce707b313be4e8cb5
ChrisMatto <christian-toni@hotmail.it>
2018-01-27 12:27:24 +0100

    BINARY

--- WebApp/src/java/courseweb/model/classi/IgwDataLayerMysqlImpl.java
+++ WebApp/src/java/courseweb/model/classi/IgwDataLayerMysqlImpl.java
@@ -69,7 +69,7 @@ public class IgwDataLayerMysqlImpl extends DataLayerMysqlImpl implements IgwData
             sCdlByMagistrale = connection.prepareStatement("SELECT * FROM CDL WHERE Magistrale=1 AND Anno=?");
             sCdlByTriennale = connection.prepareStatement("SELECT * FROM CDL WHERE Magistrale=0 AND Anno=?");
             
-            Login=connection.prepareStatement("SELECT * FROM Utente WHERE Utente.Username=? AND Utente.Password=?;");
+            Login=connection.prepareStatement("SELECT * FROM Utente WHERE BINARY Utente.Username=? AND BINARY Utente.Password=?;");
 
             sDocenti=connection.prepareStatement("SELECT IDDocente FROM Docente");
             sCorsi=connection.prepareStatement("SELECT IDCorso FROM Corso");
