1320265d384265fa086cc2006cd52fbe3c748528
Unknown <christian-toni@hotmail.it>
2018-02-20 12:57:15 +0100

    si cazzo

--- WebApp/web/js/checkSelect.js
+++ WebApp/web/js/checkSelect.js
@@ -2,6 +2,7 @@
 
 function CheckSelect(select){
   var result = [];
+  var result2=[];
   var options = select && select.options;
   var opt;
 
@@ -10,30 +11,52 @@ function CheckSelect(select){
 
     if (opt.selected) {
       result.push(opt.value);
+      result2.push(opt.value);
     }
   }
   if(result.length>0){
   
-    $.ajax({
+    var async=$.ajax({
                 url : 'change', // Your Servlet mapping or JSP(not suggested)
                 data :{result:result}, 
                 type : 'POST',
                 async: true,
                 dataType : 'html', // Returns HTML as plain text; included script tags are evaluated when inserted in the DOM.
                 success : function(data){
-                   $('.cacca').empty().end();
+                   $('.cacca1').empty().end();
                     
-                    $('.cacca').html(data).end().trigger('chzen:updated');
+                    $('.cacca1').html(data).end().trigger('chzen:updated');
                     
-                    $(".chzn-select").multipleSelect("refresh");
+                    $(".cacca1 .chzn-select").multipleSelect("refresh");
                 
                 }
                 
             });
+            
+    $.when(async).done(function(){
+        $.ajax({
+                url : 'change2', // Your Servlet mapping or JSP(not suggested)
+                data :{result:result}, 
+                type : 'POST',
+                async: true,
+                dataType : 'html', // Returns HTML as plain text; included script tags are evaluated when inserted in the DOM.
+                success : function(data){
+                   $('.cacca2').empty().end();
+                    
+                    $('.cacca2').html(data).end().trigger('chzen:updated');
+                    
+                    $(".cacca2 .chzn-select").multipleSelect("refresh");
+                
+                }
+                
+            });
+    });
         }
         else{
-            $('.cacca').empty().end();
-            $('.cacca').html("<select class='chzn-select' name='modulo' id='special-cazzafà' multiple></select>").end().trigger('chzen:updated');
+            $('.cacca1').empty().end();
+            $('.cacca1').html("<select class='chzn-select' name='propedeudici' id='special-cazzafà' multiple></select>").end().trigger('chzen:updated');
+            $('.cacca2').empty().end();
+            $('.cacca2').html("<select class='chzn-select' name='modulo' id='special-cazzafà' multiple></select>").end().trigger('chzen:updated');
             $(".chzn-select").multipleSelect("refresh");
         }
 }
